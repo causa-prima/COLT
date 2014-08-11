4cc228d47a27229a44d2cb155316d24035e3a3db
Sebastian Schultz <Sebastian2.Schultz@gmail.com>
2014-08-11 17:21:25 +0200

    - seperating base type generation from special cassandra types by subclassing - renaming base type generator class to PyRandom and subclassing from random.WichmannHill - renaming of some variables - seeding of PRNG at init or using inherited method - raising exceptions when generation of some type is not implemented - removed PRNG attribute from classes, as it is not needed after subclassing - some docstrings

--- dataGenerator.py
+++ dataGenerator.py
@@ -5,59 +5,50 @@ from decimal import Decimal
 from random import WichmannHill
 from string import printable
 from sys import float_info
-from sys import exc_info
 
 
-class Rndm:
-    def __init__(self):
-        self.PRNG = WichmannHill()
-        self.native_types_switch = dict(
-            ascii=self.pystring,
-            bigint=self.pylong,
-            blob=self.pybytearray,
-            boolean=self.pyboolean,
-            counter=self.pyint,
-            decimal=self.pydecimal,
-            double=self.pyfloat,
-            float=self.pyfloat,
-            inet=self.ip,
-            int=self.pyint,
-            text=self.pystring,
-            timestamp=self.pydate,
-            timeuuid=self.pyuuid,
+class PyRandom(WichmannHill):
+    """
+    Subclass of random.WichmannHill, implementing methods to generate some basic python types. Methods of this class
+    have 'sane' default values to support easy data generation.
+    """
+
+    def __init__(self, seed=None):
+        self.implemented_types_switch = dict(
+            date=self.pydate,
             uuid=self.pyuuid,
-            varchar=self.pystring,
-            varint=self.pylong
-        )
-        self.collections_type_switch = dict(
+            bytearray=self.pybytearray,
+            boolean=self.pyboolean,
+            string=self.pystring,
+            int=self.pyint,
+            long=self.pylong,
+            float=self.pyfloat,
+            decimal=self.pydecimal,
             list=self.pylist,
-            map=self.pydict,
+            dict=self.pydict,
             set=self.pyset
         )
 
-    def seed(self, seed):
-        """Seeds the random generator with the provided seed.
-        
-        :param hashable seed: some hashable seed 
-        """
-        self.PRNG.seed(seed)
+        if seed is not None:
+            self.seed(seed)
 
-    def generator(self, n, type, **args):
+    def generator(self, n, type_to_gen, **args):
         """Generator for all native types.
 
         :param n: number of items to generate
-        :param type: type of items to generate, must be native types
+        :param type_to_gen: type of items to generate
         :param **args: keyword args for the native type generator used
-        :return: native type generator
+        :return: type generator
         :rtype: generator
         """
         num = 0
         while num < n:
             try:
-                yield self.native_types_switch[type](**args)
+                yield self.implemented_types_switch[type_to_gen](**args)
                 num += 1
-            except:
-                print exc_info()[0]
+            except KeyError:
+                raise NotImplementedError(
+                    'Generation of type {} not implemented in {}'.format(type_to_gen, self.__class__.__name__))
 
     def getrandbits(self, bits):
         """Generates a unsigned long with maximum value of 2^bits-1.
@@ -68,19 +59,7 @@ class Rndm:
         :return: random number
         :rtype: long
         """
-        return self.PRNG.randint(0, pow(2, bits) - 1)
-
-    def ip(self, ip_type='ipv4'):
-        """Generates a random IP address.
-
-        :param ip_type: type of IP address, values except 'ipv4' generate IPv6 addresses. default = 'ipv4'
-        :return: random IP address
-        :rtype: string
-        """
-        if ip_type == 'ipv4':
-            return '.'.join([str(self.PRNG.choice(xrange(255))) for _ in xrange(4)])
-        else:
-            return ':'.join([hex(self.PRNG.choice(xrange(65535)))[2:] for _ in xrange(8)])
+        return self.randint(0, pow(2, bits) - 1)
 
     def pydate(self, start_date=None, end_date=None, start_timestamp=1388530800, end_timestamp=1420066799):
         """Generates a random timestamp between two dates given either as datetime or timestamp.
@@ -96,7 +75,7 @@ class Rndm:
         if (start_date is not None) & (end_date is not None):
             start_timestamp = int(mktime(start_date.timetuple()))
             end_timestamp = int(mktime(end_date.timetuple()))
-        return datetime.fromtimestamp(self.PRNG.randint(start_timestamp, end_timestamp))
+        return datetime.fromtimestamp(self.randint(start_timestamp, end_timestamp))
 
     def pyuuid(self):
         """Generates a random UUID.
@@ -104,20 +83,22 @@ class Rndm:
         :return: random UUID
         :rtype: uuid
         """
+
+        # code taken & adapted from standard python uuid library (/usr/lib/python2.6/uuid.py)
+
         nanoseconds = int(mktime(self.pydate().timetuple()) * 1e9)
         # 0x01b21dd213814000 is the number of 100-ns intervals between the
         # UUID epoch 1582-10-15 00:00:00 and the Unix epoch 1970-01-01 00:00:00.
         timestamp = int(nanoseconds // 100) + 0x01b21dd213814000L
-        clock_seq = self.PRNG.randrange(1 << 14L)  # instead of stable storage
+        clock_seq = self.randrange(1 << 14L)  # instead of stable storage
         time_low = timestamp & 0xffffffffL
         time_mid = (timestamp >> 32L) & 0xffffL
         time_hi_version = (timestamp >> 48L) & 0x0fffL
         clock_seq_low = clock_seq & 0xffL
         clock_seq_hi_variant = (clock_seq >> 8L) & 0x3fL
-        node = self.PRNG.randrange(1 << 47L)
+        node = self.randrange(1 << 47L)
 
         return UUID(fields=(time_low, time_mid, time_hi_version, clock_seq_hi_variant, clock_seq_low, node), version=1)
-
 
     def pybytearray(self, size=50):
         """Generates a random bytearray.
@@ -135,7 +116,7 @@ class Rndm:
         :return: random boolean
         :rtype: boolean
         """
-        return self.PRNG.randint(1, 100) <= chance_of_getting_true
+        return self.randint(1, 100) <= chance_of_getting_true
 
     def pystring(self, length=10):
         """ Generates a random string.
@@ -145,7 +126,7 @@ class Rndm:
         :rtype: string
         """
         chars = printable
-        return ''.join(self.PRNG.choice(chars) for x in range(length))
+        return ''.join(self.choice(chars) for x in range(length))
 
     def pyint(self, low=-2147483648, high=2147483647):
         """Generates a random integer.
@@ -155,7 +136,7 @@ class Rndm:
         :return: random integer
         :rtype: int or long, depending on generated number & hardware architecture
         """
-        return self.PRNG.randint(low, high)
+        return self.randint(low, high)
 
     def pylong(self, low=-1 * ((1 << 52) - 2), high=1 << 53 - 1):
         """ Generates a random long integer.
@@ -165,8 +146,8 @@ class Rndm:
         :return: random long integer
         :rtype: long
         """
-        # TODO: why these values for high and low?
-        return self.PRNG.randint(low, high)
+        # TODO: high and low cannot be bigger/smaller. why these values?
+        return self.randint(low, high)
 
     def pyfloat(self, left_digits=None, right_digits=None, positive=None):
         """ Generates a random float. Unset parameters are randomly generated.
@@ -178,13 +159,13 @@ class Rndm:
         :rtype: float
         """
 
-        left_digits = left_digits or self.PRNG.randint(1, float_info.dig)
-        right_digits = right_digits or self.PRNG.randint(0, float_info.dig - left_digits)
-        sign = 1 if positive or self.PRNG.randint(0, 1) else -1
+        left_digits = left_digits or self.randint(1, float_info.dig)
+        right_digits = right_digits or self.randint(0, float_info.dig - left_digits)
+        sign = 1 if positive or self.randint(0, 1) else -1
 
         return float("{0}.{1}".format(
-            sign * self.PRNG.randint(0, pow(10, left_digits) - 1),
-            self.PRNG.randint(0, pow(10, right_digits) - 1)
+            sign * self.randint(0, pow(10, left_digits) - 1),
+            self.randint(0, pow(10, right_digits) - 1)
         ))
 
     def pydecimal(self, left_digits=None, right_digits=None, positive=None):
@@ -199,39 +180,106 @@ class Rndm:
         return Decimal(str(self.pyfloat(left_digits, right_digits, positive)))
 
     def pylist(self, elem_count=10, elem_type='int', **elem_args):
+        """ Generates a list of definable length with items of definable type.
+
+        :param option int elem_count: length of list. default = 10
+        :param optional string elem_type: type of elements in list. default = 'int'
+        :param optional dict elem_args: keyword dict of arguments for generation of list elements
+        :return: list of length elem_count with items of type elem_type
+        :rtype: list
+        """
         result = []
         for _ in xrange(elem_count):
             try:
-                result.append(self.native_types_switch[elem_type](**elem_args))
+                result.append(self.implemented_types_switch[elem_type](**elem_args))
             except KeyError:
-                print 'generation of Datatype "{}" not supported/implemented.'.format(elem_type)
+                raise NotImplementedError(
+                    'Generation of type {} not implemented in {}'.format(type, self.__class__.__name__))
         return result
 
     def pydict(self, elem_count=10, key_type='int', elem_type='int', **elem_args):
+        """ Generates a dict of definable size with keys and items of definable type.
+
+        :param optional int elem_count: size of dict. default = 10
+        :param optional string key_type: type of dict keys. default = 'int'
+        :param optional string elem_type: type of elements in dict. default = 'int'
+        :param optional dict elem_args: keyword dict of arguments for generation of dict elements
+        :return: dict of size elem_count with keys of type key_type and items of type elem_type
+        :rtype: dict
+        """
         result = dict()
         while len(result) < elem_count:
             try:
-                result[self.native_types_switch[key_type]()] = (self.native_types_switch[elem_type](**elem_args))
+                result[self.implemented_types_switch[key_type]()] = (
+                    self.implemented_types_switch[elem_type](**elem_args))
             except KeyError:
-                print 'generation of Datatype "{}" not supported/implemented.'.format(elem_type)
+                raise NotImplementedError(
+                    'Generation of type {} not implemented in {}'.format(type, self.__class__.__name__))
         return result
 
     def pyset(self, elem_count=10, elem_type='int', **elem_args):
+        """ Generates a set of definable size with items of definable type.
+
+        :param elem_count: size of set. default = 10
+        :param elem_type: type of elements in set. default = 'int'
+        :param elem_args: keyword dict of arguments for generation of set elements
+        :return: set of size elem_count with items of type elem_type
+        :rtype: set
+        """
         result = set()
         while len(result) < elem_count:
             try:
-                result.add(self.native_types_switch[elem_type](**elem_args))
+                result.add(self.implemented_types_switch[elem_type](**elem_args))
             except KeyError:
-                print 'generation of Datatype "{}" not supported/implemented.'.format(elem_type)
+                raise NotImplementedError(
+                    'Generation of type {} not implemented in {}'.format(type, self.__class__.__name__))
         return result
 
 
-rndtest = Rndm()
-rndtest.seed(0)
+class CassandraRandom(PyRandom):
+    def __init__(self, seed=None):
+        PyRandom.__init__(self, seed)
+        new_implemented_types_switch = dict(
+            ascii=self.pystring,
+            bigint=self.pylong,
+            blob=self.pybytearray,
+            boolean=self.pyboolean,
+            counter=self.pyint,
+            decimal=self.pydecimal,
+            double=self.pyfloat,
+            float=self.pyfloat,
+            inet=self.ip,
+            int=self.pyint,
+            text=self.pystring,
+            timestamp=self.pydate,
+            timeuuid=self.pyuuid,
+            uuid=self.pyuuid,
+            varchar=self.pystring,
+            varint=self.pylong,
+            list=self.pylist,
+            map=self.pydict,
+            set=self.pyset
+        )
+        self.implemented_types_switch.update(new_implemented_types_switch)
 
-for key in rndtest.native_types_switch.keys():
-    val = rndtest.native_types_switch[key]()
-    print key
+    def ip(self, ip_type='ipv4'):
+        """Generates a random IP address.
+
+        :param ip_type: type of IP address, values except 'ipv4' generate IPv6 addresses. default = 'ipv4'
+        :return: random IP address
+        :rtype: string
+        """
+        if ip_type == 'ipv4':
+            return '.'.join([str(self.choice(xrange(255))) for _ in xrange(4)])
+        else:
+            return ':'.join([hex(self.choice(xrange(65535)))[2:] for _ in xrange(8)])
+
+
+rndtest = CassandraRandom(0)
+
+for key in rndtest.implemented_types_switch.keys():
+    val = rndtest.implemented_types_switch[key]()
+    print key, ' Type of key: ', type(key)
     print val
     if key in ('uuid', 'timeuuid'):
         print datetime.fromtimestamp((val.time - 0x01b21dd213814000L) * 100 / 1e9)
